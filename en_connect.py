78ae29987838323c7f181e769af233fc17ea8493
slambo511 <sam@samuelbancroft.com>
2017-08-28 12:57:53 +0100

    finished encode / decode

--- en_connect.py
+++ en_connect.py
@@ -1,21 +1,27 @@
 from Crypto.Cipher import AES
-obj = AES.new(b'This is a key123', AES.MODE_CBC, b'This is an IV456')
-message='The answer is no'
-b_message=message.encode()
-ciphertext = obj.encrypt(b_message)
-print(ciphertext)
-obj2 = AES.new(b'This is a key123', AES.MODE_CBC, b'This is an IV456')
-my_password = ""
-print(obj2.decrypt(ciphertext).decode("utf-8"))
 
 
-def crypto_this(input_plain, key_text, salt_text):
+def crypto_this(input_plain, key_text, iv_text):
     input_b = input_plain.encode()
     key_text_b = key_text.encode()
-    salt_text_b = salt_text.encode()
-    obj_en = AES.new(key_text_b, AES.MODE_OFB, salt_text_b)
+    iv_text_b = iv_text.encode()
+    obj_en = AES.new(key_text_b, AES.MODE_OFB, iv_text_b)
     to_encrypt = obj_en.encrypt(input_b)
     return to_encrypt
 
-encrypted_text = crypto_this("test_user, test_db, password", "This is a key123", "This is an IV456")
+
+def decrypto_this(input_crypto, key_text, iv_text):
+    input_b = input_crypto
+    key_text_b = key_text.encode()
+    iv_text_b = iv_text.encode()
+    obj_de = AES.new(key_text_b, AES.MODE_OFB, iv_text_b)
+    to_decrypt = obj_de.decrypt(input_b)
+    return to_decrypt.decode("utf-8")
+
+stored_info = "test_user, test_db, password"
+key = "This is a key123"
+iv = "This is an IV456"
+encrypted_text = crypto_this(stored_info, key, iv)
 print(encrypted_text)
+decrypted_text = decrypto_this(encrypted_text, key, iv)
+print(decrypted_text)
